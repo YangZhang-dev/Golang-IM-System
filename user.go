8b44e245cbc57c082b5dadc1f7a02b762ca8ff68
zzys <2803436425@qq.com>
2022-05-11 17:01:55 +0800

    v0.4

--- user.go
+++ user.go
@@ -1,6 +1,8 @@
 package main
 
-import "net"
+import (
+	"net"
+)
 
 type User struct {
 	Username string
@@ -32,14 +34,49 @@ func (u *User) Offline() {
 	u.Server.Maplock.Unlock()
 }
 
-//发送消息
+//用户端返回给自己系统提示
+func (u *User) SendMessage(message Message) {
+	u.coon.Write([]byte(message.Mess))
+}
+
+//处理消息
 func (u *User) DoMessage(mes string) {
+	var message Message
+
 	if mes == "" {
 		return
+	} else if mes == "who" {
+		//如果是who则应该返回所有在UserOline的用户
+		u.Server.Maplock.Lock()
+		for _, user := range u.Server.Useronline {
+			if user != u {
+				message = GetSysMes("["+user.Username+"]"+"在线中\n", u)
+				u.SendMessage(message)
+			}
+		}
+		u.Server.Maplock.Unlock()
+	} else if len(mes) > 7 && mes[:7] == "rename|" {
+		//更改用户名
+		newusername := mes[7:]
+		_, ok := u.Server.Useronline[newusername]
+		if ok {
+			message = GetSysMes("用户名已经存在\n", u)
+
+		} else {
+			u.Server.Broatcast(u, "我改名为"+newusername)
+			u.Server.Maplock.Lock()
+			delete(u.Server.Useronline, u.Username)
+			u.Server.Useronline[newusername] = u
+			u.Server.Maplock.Unlock()
+			u.Username = newusername
+			message = GetSysMes("成功改名为:"+u.Username+"\n", u)
+		}
+		u.SendMessage(message)
+	} else {
+		//广播消息
+		u.Server.Broatcast(u, mes)
 	}
-	mess := "[" + u.Addr + "]" + u.Username + "说:" + mes
-	message := Message{Mess: mess, User: u}
-	u.Server.Mes <- message
+
 }
 
 //在server端accapt一个请求后，创建一个User
@@ -55,4 +92,13 @@ func NewUser(coon net.Conn, server Server) *User {
 	//不断监听User的chan
 	go user.ListenMessage()
 	return user
+}
+
+func GetSysMes(mes string, user *User) Message {
+	return Message{Mess: mes, User: user}
+}
+
+func GetMessage(mes string, user *User) Message {
+	mess := "[" + user.Addr + "]" + user.Username + "说:" + mes
+	return Message{Mess: mess, User: user}
 }
